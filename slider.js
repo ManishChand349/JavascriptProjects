6937c4d0e5e7c49d6a2211e5bfe66042fc926549
Manish Chand <manishchandr349@gmail.com>
2023-07-11 10:14:52 +0530

    hello

--- slider.js
+++ slider.js
@@ -1,17 +1,35 @@
-const left = document.querySelector(".left")
-const right = document.querySelector(".right")
-const slider = document.querySelector(".slider")
-const images = document.querySelectorAll(".image")
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".image");
 
 let sliderNumber = 1;
+const length = images.length;
 
+const nextSlide = () => {
+  slider.style.transform = `translateX(-${sliderNumber * 500}px)`;
+  sliderNumber++;
+};
+const prevSlide = () => {
+  slider.style.transform = `translateX(-${(sliderNumber-2) * 500}px)`;
+  sliderNumber--;
+};
 
-right.addEventListener("click", () =>{
-     if(sliderNumber < images.length){
-          slider.style.transform = `translateX(-${sliderNumber * 500}px)`;
-          sliderNumber++;
-     } else{
-          slider.style.transform = `translateX(0px)`
-          sliderNumber = 1;
-     }
+const getFirstSlider = () => {
+  slider.style.transform = `translateX(0px)`;
+  sliderNumber = 1;
+};
+const getLastSlider = () => {
+  slider.style.transform = `translateX(-${(length-1) * 500}px`;
+  sliderNumber = 1;
+};
+
+right.addEventListener("click", () => {
+  sliderNumber < length ? nextSlide() : getFirstSlider();
+});
+
+left.addEventListener("click", ()=>{
+     sliderNumber > 1 ? prevSlide() : getLastSlider()
+
 })
+
