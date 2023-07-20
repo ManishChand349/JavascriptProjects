9acecae2883bccc8a7749a4c96289509e6786c7d
Manish Chand <manishchandr349@gmail.com>
2023-07-20 19:59:19 +0530

    change

--- slider.js
+++ slider.js
@@ -2,16 +2,42 @@ const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const slider = document.querySelector(".slider");
 const images = document.querySelectorAll(".image");
+const bottom = document.querySelector(".bottom");
 
 let sliderNumber = 1;
 const length = images.length;
+
+for (let i = 0; i < length; i++) {
+     const div = document.createElement("div");
+     div.className = "button";
+     bottom.appendChild(div);
+   }
+   
+   const buttons = document.querySelectorAll(".button");
+   buttons[0].style.backgroundColor = "white";
+   
+   const resetBg = () => {
+     buttons.forEach((button) => {
+       button.style.backgroundColor = "transparent";
+     });
+   };
+   
+   buttons.forEach((button, i) => {
+     button.addEventListener("click", () => {
+       resetBg();
+       slider.style.transform = `translateX(-${i * 500}px)`;
+       sliderNumber = i +1;
+       button.style.backgroundColor = "white";
+     });
+   });
+   
 
 const nextSlide = () => {
   slider.style.transform = `translateX(-${sliderNumber * 500}px)`;
   sliderNumber++;
 };
 const prevSlide = () => {
-  slider.style.transform = `translateX(-${(sliderNumber-2) * 500}px)`;
+  slider.style.transform = `translateX(-${(sliderNumber - 2) * 500}px)`;
   sliderNumber--;
 };
 
@@ -20,16 +46,24 @@ const getFirstSlider = () => {
   sliderNumber = 1;
 };
 const getLastSlider = () => {
-  slider.style.transform = `translateX(-${(length-1) * 500}px`;
-  sliderNumber = 1;
+  slider.style.transform = `translateX(-${(length - 1) * 500}px`;
+  sliderNumber = length;
 };
+  
+const changeColor = ()=>{
+     resetBg()
+     buttons[sliderNumber-1].style.backgroundColor = "white"
 
+}
 right.addEventListener("click", () => {
   sliderNumber < length ? nextSlide() : getFirstSlider();
+  changeColor()
+    
 });
 
-left.addEventListener("click", ()=>{
-     sliderNumber > 1 ? prevSlide() : getLastSlider()
+left.addEventListener("click", () => {
+     sliderNumber > 1 ? prevSlide() : getLastSlider();
+     changeColor()
 
-})
+});
 
